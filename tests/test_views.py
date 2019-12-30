3039bc471175c1f047f32e2bbdc78ea9b63f3c07
Luiz Henrique Longo <longo.luizh@gmail.com>
2019-12-30 11:12:00 -0300

    update tests views

--- tests/test_views.py
+++ tests/test_views.py
@@ -10,14 +10,14 @@ from app import lecture_manager
 
 def test_no_body_request():
     response = requests.post(
-        url='http://127.0.0.1:5000/api/v1/lecture_manager',
+        url='https://lecture-manager.herokuapp.com/api/v1/lecture_manager',
         data=None
         )
     assert response.status_code == 400
 
 def test_not_dict_body_request():
     response = requests.post(
-        url='http://127.0.0.1:5000/api/v1/lecture_manager',
+        url='https://lecture-manager.herokuapp.com/api/v1/lecture_manager',
         data=json.dumps(['1', '2', '3']),
         headers={'Content-type': 'application/json'}
         )
@@ -25,7 +25,7 @@ def test_not_dict_body_request():
 
 def test_empty_body_request():
     response = requests.post(
-        url='http://127.0.0.1:5000/api/v1/lecture_manager',
+        url='https://lecture-manager.herokuapp.com/api/v1/lecture_manager',
         data=json.dumps({"data": []}),
         headers={'Content-type': 'application/json'}
         )
@@ -33,7 +33,7 @@ def test_empty_body_request():
 
 def test_not_dict_data_request():
     response = requests.post(
-        url='http://127.0.0.1:5000/api/v1/lecture_manager',
+        url='https://lecture-manager.herokuapp.com/api/v1/lecture_manager',
         data=json.dumps({"data": {'1':1, '2':2, '3':3}}),
         headers={'Content-type': 'application/json'}
         )
@@ -41,7 +41,7 @@ def test_not_dict_data_request():
 
 def test_incorrect_data_format():
     response = requests.post(
-        url='http://127.0.0.1:5000/api/v1/lecture_manager',
+        url='https://lecture-manager.herokuapp.com/api/v1/lecture_manager',
         data=json.dumps({"data": ['this data shall not pass!']}),
         headers={'Content-type': 'application/json'}
         )
@@ -49,7 +49,7 @@ def test_incorrect_data_format():
 
 def test_enough_data():
     response = requests.post(
-        url='http://127.0.0.1:5000/api/v1/lecture_manager',
+        url='https://lecture-manager.herokuapp.com/api/v1/lecture_manager',
         data=json.dumps({"data": [
             "Writing Fast Tests Against Enterprise Rails 15min",
             "Overdoing it in Python 45min"
